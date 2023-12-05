330b8a78654cfa1f24e7f8d3527700860757cd7c
solamsalu <solamsalu@gmail.com>
2023-12-04 23:43:10 -0800

    Task 0

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.error(err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return await getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
